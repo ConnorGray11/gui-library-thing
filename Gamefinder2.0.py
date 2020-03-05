e677cbcde2efeb28ec068beda6b8a64e561c8659
Connor T Gray <cgray@debian07.CIS.COM>
2020-03-05 10:02:26 -0600

    xdsss

--- Gamefinder2.0.py
+++ Gamefinder2.0.py
@@ -9,7 +9,7 @@ import pickle
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 from tkinter import messagebox
-
+from tkinter import *
 
 #class
 class Screen(tk.Frame):
@@ -101,14 +101,64 @@ class MainMenu(Screen):
          #so in case other people use this this was made and designed by connor Gray 
          
          #you can search stuff up here
+         
+
+     
+class Error(tk.Frame):
+        def __init__(self,parent):
+                tk.Frame.__init__(self,master=parent)
+                self.parent = parent
+                self.lbl_continue = tk.Label(self,text="Select A diddly dang friggin title you ingrate")
+                self.lbl_continue.grid(row=0, column=0)
+                
+                self.btn_ok=tk.Button(self,text="yeet",command = self.parent.destroy)
+                self.btn_ok.grid(row=1,column=0)
+                
+                  
 class Search(Screen):
         def __init__(self):
                 Screen.__init__(self)
                 
-                self.options=["one","two"]
+                self.selected=tk.StringVar(self)
+                self.options=["all","genre","title","Developer","Publisher","System","Rating","Release","Coop","Price","Beaten","Purchase date"]
+               # self.dbx
+                
+                #self.options=[games]
                 self.tkvar=tk.StringVar(self)
-                self.tkvar.set(self.options[0])
+                
+                self.title_var = BooleanVar(self)
+                self.title_var.set(True)
 
+                self.dev_var = BooleanVar(self)
+                self.dev_var.set(True)
+
+                self.pub_var = BooleanVar(self)
+                self.pub_var.set(True)
+
+                self.system_var = BooleanVar(self)
+                self.system_var.set(True)
+
+                self.rating_var = BooleanVar(self)
+                self.rating_var.set(True)
+
+                self.release_var = BooleanVar(self)
+                self.release_var.set(True)
+
+                self.coop_var = BooleanVar(self)
+                self.coop_var.set(True)
+
+                self.price_var = BooleanVar(self)
+                self.price_var.set(True)
+
+                self.beaten_var = BooleanVar(self)
+                self.beaten_var.set(True)
+
+                self.pur_var = BooleanVar(self)
+                self.pur_var.set(True)
+
+                self.genre_var = BooleanVar(self)
+                self.genre_var.set(True)
+                
                 self.lbl_title = tk.Label(self, text="Search",font=("Arial","25"))
                 self.lbl_title.grid(row=0,column=0,sticky="news") 
                 
@@ -129,30 +179,29 @@ class Search(Screen):
                 self.filters = tk.Label(self,text="Print Filters:", font=("arial","18"))
                 self.filters.grid(row=1,column=2,sticky="news")
                 
-                self.chk1 = tk.Checkbutton( self,text='Title')
+                self.chk1 = tk.Checkbutton( self,text='Genre', variable = self.genre_var,selectcolor="black")
                 self.chk1.grid(row=1,column=4)
-                self.chk2 = tk.Checkbutton( self,text='Genre')
+                self.chk2 = tk.Checkbutton( self,text='Title',variable = self.title_var,selectcolor="black")
                 self.chk2.grid(row=2,column=4)
-                self.chk3 = tk.Checkbutton( self,text='Developer')
+                self.chk3 = tk.Checkbutton( self,text='Developer',variable = self.dev_var,selectcolor="black")
                 self.chk3.grid(row=3,column=4)
-                self.chk4 = tk.Checkbutton( self,text='Publisher')
+                self.chk4 = tk.Checkbutton( self,text='Publisher',variable = self.pub_var,selectcolor="black")
                 self.chk4.grid(row=4,column=4)  
-                self.chk5 = tk.Checkbutton( self,text='System')
+                self.chk5 = tk.Checkbutton( self,text='System',variable = self.system_var,selectcolor="black")
                 self.chk5.grid(row=1,column=5)
-                self.chk6 = tk.Checkbutton( self,text='Release')
+                self.chk6 = tk.Checkbutton( self,text='Release',variable = self.release_var,selectcolor="black")
                 self.chk6.grid(row=2,column=5)  
-                self.chk7 = tk.Checkbutton( self,text='Rating')
+                self.chk7 = tk.Checkbutton( self,text='Rating',variable = self.rating_var,selectcolor="black")
                 self.chk7.grid(row=3,column=5)
-                self.chk8 = tk.Checkbutton( self,text='Co-op')
+                self.chk8 = tk.Checkbutton( self,text='Co-op',variable = self.coop_var,selectcolor="black")
                 self.chk8.grid(row=4,column=5)   
-                self.chk9 = tk.Checkbutton( self,text='Price')
+                self.chk9 = tk.Checkbutton( self,text='Price',variable = self.price_var,selectcolor="black")
                 self.chk9.grid(row=1,column=6)
-                self.chk10 = tk.Checkbutton( self,text='Beaten')
+                self.chk10 = tk.Checkbutton( self,text='Beaten',variable = self.beaten_var,selectcolor="black")
                 self.chk10.grid(row=2,column=6)  
-                self.chk11 = tk.Checkbutton( self,text='Purchase')
+                self.chk11 = tk.Checkbutton( self,text='Purchase',variable = self.pur_var,selectcolor="black")
                 self.chk11.grid(row=3,column=6)
-                  
-            
+                
                               
                 
                 self.edit_space = ScrolledText(self,
@@ -176,27 +225,171 @@ class Search(Screen):
                 self.btn_cancel=tk.Button(self,text="Cancel",font=("Arial","15"),command= self.go_home)
                 self.btn_cancel.grid(row=6,column=0)
                 
-                self.btn_clear=tk.Button(self,text="Clear",font=("Arial","15"))
+                self.btn_clear=tk.Button(self,text="Clear",font=("Arial","15"),command = self.clear)
                 self.btn_clear.grid(row=6,column=1)
                 
-                self.btn_submit=tk.Button(self,text="Submit",font=("Arial","15"))
+                self.btn_submit=tk.Button(self,text="Submit",font=("Arial","15"), command = self.print_search)
                 self.btn_submit.grid(row=6,column=3) 
                 
                 
-                                 
                 
+        
+                        
+                
+        
                 self.grid_columnconfigure(0,weight=1)
                 self.grid_columnconfigure(1,weight=1)
                 self.grid_columnconfigure(2,weight=1)                 
                 self.grid_columnconfigure(3,weight=1)
             
+        def print_search(self):
+                self.edit_space.delete(0.0,"end")
+                keyword = self.ent_search.get()
+                for key in games.keys():
+                        entry=games[key]
+                        
+                        if self.tkvar.get() == "all":
+                                self.filter_check(entry)
+                                        
+                        if self.tkvar.get() == "genre":
+                                if keyword in entry[0]:
+                                        self.filter_check(entry)    
+                                        
+                        if self.tkvar.get() == "title":
+                                if keyword in entry[1]:
+                                        self.filter_check(entry) 
+                                        
+                        if self.tkvar.get() == "Developer":
+                                if keyword in entry[2]:
+                                        self.filter_check(entry) 
+                                                
+                        if self.tkvar.get() == "Publisher":
+                                if keyword in entry[3]:
+                                        self.filter_check(entry) 
+                                                
+                        if self.tkvar.get() == "System":
+                                if keyword in entry[4]:
+                                        self.filter_check(entry) 
+                                                                                        
+                        if self.tkvar.get() == "Rating":
+                                if keyword in entry[6]:
+                                        self.filter_check(entry)   
+                                        
+                        if self.tkvar.get() == "Release":
+                                if keyword in entry[5]:
+                                        self.filter_check(entry)   
+                                                
+                        if self.tkvar.get() == "Coop":
+                                if keyword in entry[7]:
+                                        self.filter_check(entry)   
+                                                        
+                        if self.tkvar.get() == "Price":
+                                if keyword in entry[8]:
+                                        self.filter_check(entry)   
+                                                                                
+                        if self.tkvar.get() == "Beaten":
+                                if keyword in entry[9]:
+                                        self.filter_check(entry)
+                                        
+                        if self.tkvar.get() == "Purchase Date":
+                                if keyword in entry[10]:
+                                        self.filter_check(entry)                                           
+                
+          
+                          
+                        
+        def filter_check(self, entry):
+                    
+                        if self.genre_var.get() == True:
+                                msg=entry[0]+"\n"
+                                self.edit_space.insert("insert", msg)   
+                                
+                        if self.title_var.get() == True:
+                                msg=entry[1]+"\n"
+                                self.edit_space.insert("insert", msg)   
+                                
+                        if self.dev_var.get() == True:
+                                msg=entry[2]+"\n"
+                                self.edit_space.insert("insert", msg)   
+                                
+                        if self.pub_var.get() == True:
+                                msg=entry[3]+"\n"
+                                self.edit_space.insert("insert", msg)   
+                                                
+                        if self.system_var.get() == True:
+                                msg=entry[4]+"\n"
+                                self.edit_space.insert("insert", msg)    
+                                
+                        if self.release_var.get() == True:
+                                msg=entry[5]+"\n"
+                                self.edit_space.insert("insert", msg)    
+                                
+                        if self.rating_var.get() == True:
+                                msg=entry[6]+"\n"
+                                self.edit_space.insert("insert", msg)    
+                                                
+                        if self.coop_var.get() == True:
+                                msg=entry[7]+"\n"
+                                self.edit_space.insert("insert", msg)    
+                                
+                        if self.price_var.get() == True:
+                                msg=entry[8]+"\n"
+                                self.edit_space.insert("insert", msg)    
+                                        
+                        if self.beaten_var.get() == True:
+                                msg=entry[9]+"\n"
+                                self.edit_space.insert("insert", msg)    
+                                                
+                        if self.pur_var.get() == True:
+                                msg=entry[10]+"\n"
+                                self.edit_space.insert("insert", msg)  
+                                        
+                        msg ="++++++++++++++++++++++++++++\n"
+                        self.edit_space.insert("insert", msg)
+                               
+                        
+                        
+        def clear(self):
+                self.edit_space.delete(0.0,"end")
+                
+                self.title_var.set(False)
+        
+                self.dev_var.set(False)
+        
+                self.pub_var.set(False)
+    
+                self.system_var.set(False)
+      
+                self.rating_var.set(False)
+
+                self.release_var.set(False)
+   
+                self.coop_var.set(False)
+      
+                self.pu
+                self.price_var.set(False)
+    
+                self.beaten_var.set(False)
+   
+                self.pur_var.set(False)
+       
+                self.genre_var.set(False)                
+                
+        
         def go_home(self):
+                #self.edit_space.delete(0.0,"end")
                 Screen.current=0
                 Screen.switch_frame()    
                 
                 
-   #so in case other people use this this was made and designed by connor Gray 
-   #you can add files to the program here
+       
+                
+  #so in case other people use this this was made and designed by connor Gray 
+  #you can add files to the program here
+  
+
+   
+
 class Add(Screen):
         def __init__(self):
                 Screen.__init__(self)
@@ -399,11 +592,16 @@ class Edit_Select(tk.Frame):
                 
         def go_edit(self):
                 if self.tkvar.get()==self.options[0]:
-                        pass
+                        popup=tk.Tk()
+                        popup.title("yeet")
+                        msg="error"
+                        frm_error=Error(popup)
+                        frm_error.grid(row=0,column=0)  
                 else:
                         Screen.current=3
                         if self.tkvar.get == self.options:
                                 pass
+                                                      
                         else:
                                
                                 for i in range(len(self.options)):
@@ -412,8 +610,8 @@ class Edit_Select(tk.Frame):
                                                 screens[3].edit_key=i
                                                 screens[3].edit_update()
                                                 break                        
-                Screen.switch_frame()
-                self.parent.destroy()
+                                        Screen.switch_frame()
+                                        self.parent.destroy()
                 
                         
  #so in case other people use this this was made and designed by connor Gray
